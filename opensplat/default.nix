5ce825389e3f336ef43eafd348cb0edee3be8e8a
Julius Michaelis <michaelis@jp.fujitsu.com>
2024-04-13 20:16:59 +0900

    opensplat works

--- opensplat/default.nix
+++ opensplat/default.nix
@@ -5,7 +5,7 @@ let
 in
   {
     lib,
-    stdenv,
+    gcc12Stdenv,
     cmake,
     fetchFromGitHub,
     python3,
@@ -18,9 +18,15 @@ in
     # In theory, this also supports rocm/hip, but I've been unable to get that build to work
     useCuda ? config.cudaSupport,
     cudaPackages,
+    autoAddDriverRunpath,
     cmakeFlags ? [], # There's some interesting flags like OPENSPLAT_BUILD_SIMPLE_TRAINER or OPENSPLAT_MAX_CUDA_COMPATIBILITY
-  }:
-    stdenv.mkDerivation {
+  }: let
+    torch = python3.pkgs.torch.override {cudaSupport = useCuda;};
+  in
+    # Using a normal stdenv gives
+    # ld: /nix/store/k1l7y96gv0nc685cg7i3g43i4icmddzk-python3.11-torch-2.2.1-lib/lib/libc10.so: undefined reference to `std::ios_base_library_init()@GLIBCXX_3.4.32'
+    # Apparently, nvcc/cudatoolkit bring their own gcc. Look for CMAKE_CUDA_HOST_COMPILER in the --keep-failed folder if you see a similar linking failure
+    gcc12Stdenv.mkDerivation {
       pname = name;
       version = version;
 
@@ -43,11 +49,13 @@ in
         [cmake]
         ++ lib.optionals useCuda [
           cudaPackages.cuda_nvcc
+          autoAddDriverRunpath
         ];
 
       buildInputs =
         [
-          (python3.pkgs.torch.override { cudaSupport = useCuda; })
+          torch.cxxdev
+          torch
           opencv
           nanoflann
           nlohmann_json
@@ -57,7 +65,7 @@ in
         ++ lib.optionals useCuda [
           cudaPackages.cuda_cudart
         ];
-      env.VERBOSE = "1";
+      env.TORCH_CUDA_ARCH_LIST = "${lib.concatStringsSep ";" python3.pkgs.torch.cudaCapabilities}";
 
       cmakeFlags =
         [
@@ -68,8 +76,6 @@ in
           "-DCUDA_TOOLKIT_ROOT_DIR=${cudaPackages.cudatoolkit}"
           # To avoid: forward.cu(411): error: calling a constexpr __host__ function("min") from a __device__ function("project_cov3d_ewa") is not allowed. The experimental flag '--expt-relaxed-constexpr' can be used to allow this.
           "-DCMAKE_CUDA_FLAGS=--expt-relaxed-constexpr"
-          # To avoid: /nix/store/imajhva2ms4zci9gr2znhij3bdbwdm3i-libtorch-2.0.0-dev/include/c10/cuda/CUDAMacros.h:8:10: fatal error: c10/cuda/impl/cuda_cmake_macros.h: No such file or directory
-          "-DCMAKE_CXX_FLAGS=-DC10_CUDA_NO_CMAKE_CONFIGURE_FILE"
         ]
         ++ cmakeFlags;
 
