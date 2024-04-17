2bdba55509298abc536c3973332241d63e3e305d
Julius Michaelis <michaelis@jp.fujitsu.com>
2024-04-17 21:56:28 +0900

    frob

--- opensplat/default.nix
+++ opensplat/default.nix
@@ -6,7 +6,9 @@ in
   {
     lib,
     gcc12Stdenv,
+    stdenv,
     cmake,
+    ninja,
     fetchFromGitHub,
     python3,
     opencv,
@@ -15,18 +17,24 @@ in
     glm,
     cxxopts,
     config,
-    # In theory, this also supports rocm/hip, but I've been unable to get that build to work
-    useCuda ? config.cudaSupport,
+    # Upstream has rocm/hip support, too. anyone?
+    cudaSupport ? config.cudaSupport,
     cudaPackages,
     autoAddDriverRunpath,
     cmakeFlags ? [], # There's some interesting flags like OPENSPLAT_BUILD_SIMPLE_TRAINER or OPENSPLAT_MAX_CUDA_COMPATIBILITY
   }: let
-    torch = python3.pkgs.torch.override {cudaSupport = useCuda;};
-  in
-    # Using a normal stdenv gives
+    torch = python3.pkgs.torch.override {inherit cudaSupport;};
+    # Using a normal stdenv with cuda torch gives
     # ld: /nix/store/k1l7y96gv0nc685cg7i3g43i4icmddzk-python3.11-torch-2.2.1-lib/lib/libc10.so: undefined reference to `std::ios_base_library_init()@GLIBCXX_3.4.32'
-    # Apparently, nvcc/cudatoolkit bring their own gcc. Look for CMAKE_CUDA_HOST_COMPILER in the --keep-failed folder if you see a similar linking failure
-    gcc12Stdenv.mkDerivation {
+    mkDerivation =
+      (
+        if cudaSupport
+        then gcc12Stdenv
+        else stdenv
+      )
+      .mkDerivation;
+  in
+    mkDerivation {
       pname = name;
       version = version;
 
@@ -38,44 +46,37 @@ in
       };
       patches = [
         ./install-executables.patch
-        ./unvendor-some.patch # the two vendored gsplats are so heavily modified they may be considered a fork
       ];
-      prePatch = ''
-        # unvendor only what is already packaged in nix. todo: package more
-        rm -rf vendor/{glm,json,nanoflann,cxxopts.hpp}
+      postPatch = ''
+        # the two vendored gsplats are so heavily modified they may be considered a fork
+        find vendor ! -name 'gsplat*' -maxdepth 1 -mindepth 1 -exec rm -rf {} +
+        mkdir vendor/{nanoflann,glm}
+        ln -s ${glm}/include/glm vendor/glm/glm
+        ln -s ${nanoflann}/include/nanoflann.hpp vendor/nanoflann/nanoflann.hpp
+        ln -s ${nlohmann_json}/include/nlohmann vendor/json
+        ln -s ${cxxopts}/include/cxxopts.hpp vendor/cxxopts.hpp
       '';
 
       nativeBuildInputs =
-        [cmake]
-        ++ lib.optionals useCuda [
+        [cmake ninja]
+        ++ lib.optionals cudaSupport [
           cudaPackages.cuda_nvcc
           autoAddDriverRunpath
         ];
-
       buildInputs =
         [
+          nlohmann_json
           torch.cxxdev
           torch
           opencv
-          nanoflann
-          nlohmann_json
-          glm
-          cxxopts
         ]
-        ++ lib.optionals useCuda [
-          cudaPackages.cuda_cudart
-        ];
+        ++ lib.optionals cudaSupport [cudaPackages.cuda_cudart];
       env.TORCH_CUDA_ARCH_LIST = "${lib.concatStringsSep ";" python3.pkgs.torch.cudaCapabilities}";
-
       cmakeFlags =
-        [
-          "-DCMAKE_SKIP_RPATH=true"
-        ]
-        ++ lib.optionals useCuda [
+        ["-DCMAKE_SKIP_RPATH=true"]
+        ++ lib.optionals cudaSupport [
           "-DGPU_RUNTIME=CUDA"
           "-DCUDA_TOOLKIT_ROOT_DIR=${cudaPackages.cudatoolkit}"
-          # To avoid: forward.cu(411): error: calling a constexpr __host__ function("min") from a __device__ function("project_cov3d_ewa") is not allowed. The experimental flag '--expt-relaxed-constexpr' can be used to allow this.
-          "-DCMAKE_CUDA_FLAGS=--expt-relaxed-constexpr"
         ]
         ++ cmakeFlags;
 
@@ -83,6 +84,7 @@ in
         description = "Production-grade 3D gaussian splatting";
         homepage = "https://github.com/${owner}/${name}";
         license = lib.licenses.mit;
-        platforms = lib.platforms.unix;
+        platforms = lib.platforms.linux ++ lib.optionals (!cudaSupport) lib.platforms.darwin;
+        maintainers = [lib.maintainers.jcaesar];
       };
     }
